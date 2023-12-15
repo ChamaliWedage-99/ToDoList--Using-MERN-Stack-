ee6047957c02ed99d03e6719e309cff0e7f94113
Chamali Wedage <94621140+ChamaliWedage-99@users.noreply.github.com>
2023-12-15 17:33:47 +0530

    first commit

---
+++ todolist/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Home from './Home'
+
+function App() {
+
+
+  return (
+   <div>
+    <h2>To Do List</h2>
+   </div>
+  )
+}
+
+export default App
